f2215ae4c708842bc485651316d1397dcf7cb280
Marcellus Von Sacramento <marcellus.sacramento@gmail.com>
2025-08-13 01:32:49 -0700

    Changed sensor level variable name.

--- misc-headers/esp-now-message-struct.h
+++ misc-headers/esp-now-message-struct.h
@@ -8,7 +8,7 @@ Purpose: This header file will contain various custom esp-message structure as n
 #define ESP_NOW_MESSAGE_STRUCT
 
 typedef struct esp_message {
-	uint8_t sensor_status_flag;
+	uint8_t sensor_read_level;
 	char message[101];
 } esp_message;
 
