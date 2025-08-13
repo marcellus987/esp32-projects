88356171802cc4f487badea80ca12fe30e87b6f6
Marcellus Von Sacramento <marcellus.sacramento@gmail.com>
2025-08-12 22:39:01 -0700

    Implement tracker for subsquent sends for enetering deep-sleep.

--- esp-now-slave-device/src/main.c
+++ esp-now-slave-device/src/main.c
@@ -26,7 +26,10 @@ Purpose: This source code is meant to be uploaded to Master ESP32 device.
 #define RELEASE_BUILD_SLEEP_TIME 43200000000 /* 43,200,000,000 == 12 hours. */
 #define TEST_BUILD_SLEEP_TIME 5000000 /* 5000000 == 5 seconds. */
 
-
+typedef enum send_counter {
+    INITIAL_SEND, 
+    SUBSEQUENT_SEND
+} send_counter_t;
 
 
 /* Callback function prototype. */
@@ -39,12 +42,14 @@ void onReceived(const esp_now_recv_info_t *peer_info, const uint8_t *data_receiv
 int message_count = 0;
 uint8_t master[ESP_NOW_ETH_ALEN] = {0x88, 0x13, 0xbf, 0x0b, 0xe1, 0x50};
 esp_netif_t *netif_wifi_sta;
+send_counter_t send_counter_state; /* Callback function registered for sending should be the only one to update this. */
 
 
 
 /* Setup. */
 
 bool initWiFi() {
+    printf("initWiFi() call entry...\n");
     esp_err_t err = nvs_flash_init();
 
     // Initialize NVS flash.
@@ -64,20 +69,30 @@ bool initWiFi() {
     ESP_ERROR_CHECK(esp_wifi_start()); // Start wifi in set mode.
     ESP_ERROR_CHECK(esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE));
     ESP_ERROR_CHECK(esp_wifi_disconnect()); // Disconnect to ensure d/'evice does not auto-connect to AP or other peer.
+   
+    /* Set send counter state. */
+    send_counter_state = INITIAL_SEND;
+    printf("initWiFi() call exit...\n");
     
     return true;
 }
 
 bool initESPNOW() {
+    printf("initESPNOW() call entry...\n");
+
     ESP_ERROR_CHECK(esp_now_init());
 
     /* Register callback functions. */
     ESP_ERROR_CHECK(esp_now_register_send_cb(onSent));
     ESP_ERROR_CHECK(esp_now_register_recv_cb(onReceived));
+    printf("initESPNOW() call exit...\n");
+
     return true;
 }
 
 void pinConfig() { 
+    printf("pinConfig() call entry...\n");
+
     gpio_config_t cfg;
     cfg.pin_bit_mask = (1ULL << GPIO_NUM_26 | 1ULL << GPIO_NUM_27); /* Pins used by RED_LED_PIN and GREEN_LED_PIN. */
     cfg.mode = GPIO_MODE_OUTPUT;
@@ -95,9 +110,15 @@ void pinConfig() {
     gpio_set_direction(SENSOR_PIN, GPIO_MODE_INPUT);
     gpio_set_pull_mode(SENSOR_PIN, GPIO_PULLUP_ONLY);
     gpio_pullup_en(SENSOR_PIN);
+
+    printf("pinConfig() call exit...\n");
+
 } // End of pinConfig().
 
 void sleepConfig() {
+    printf("sleepConfig() call entry...\n");
+
+
     /* Turn ON then OFF all Power Domains (PDs). Based on my current knowledge of 
        this is just for avoiding assertion when ref >= 0 is false for any of the PDs.
        Turning is OFF sets ref == 0 which makes it eligible for ESP_PD_OPTION_OFF since
@@ -116,6 +137,9 @@ void sleepConfig() {
 
     // Enable RTC timer as wakeup source.
     esp_sleep_enable_timer_wakeup(TEST_BUILD_SLEEP_TIME);
+
+    printf("sleepConfig() call exit...\n");
+
 } // End of sleepConfig().
 
 
@@ -124,21 +148,51 @@ void sleepConfig() {
 /* Send callback function. */
 
 void onSent(const esp_now_send_info_t *peer_info, esp_now_send_status_t status) {
+    printf("onSent() call entry...\n");
+    char *send_counter_state_desc = send_counter_state == INITIAL_SEND ? "Initial" : "Subsequent";
+
     if(status == ESP_NOW_SEND_SUCCESS) {
-        printf("Data delivered successfully and Master received the data.\n\n");
+        printf("\n\n%s send succeed.\n\n", send_counter_state_desc);
+        
     }
     else {
-        printf("Send fail.\n\n");
+        printf("\n\n%s Send fail.\n\n", send_counter_state_desc);
     }
+
+    /* To prepare for deep-sleep regardless of delivery state. */
+    if(send_counter_state == SUBSEQUENT_SEND) {
+        esp_wifi_stop();
+        esp_netif_deinit();
+        esp_netif_destroy_default_wifi(netif_wifi_sta);
+
+        /* Configure sleep mode to be used. */
+        printf("Configuring sleep mode...\n");
+        sleepConfig();
+        
+        /* For debug. */
+        printf("\n\nEntering Deep-sleep mode.\n\n");
+        ESP_ERROR_CHECK(esp_deep_sleep_try_to_start());
+    }
+
+    // Updates only after initial send.
+    if(send_counter_state == INITIAL_SEND) {
+        send_counter_state = SUBSEQUENT_SEND;
+    }
+    
+    printf("onSent() call exit...\n");
 }
 
 void onReceived(const esp_now_recv_info_t *peer_info, const uint8_t *data_received, int data_len) {
+    printf("onReceived() call entry...\n");
+
     printf("\nReceived from:\n");
     printf("Sender MAC address: " MACSTR "\n", MAC2STR(peer_info->src_addr));
     
     // This is sizeof(data_received) - 1 because null char is not included.
     printf("Message length: %d\n", strlen((const char*)data_received));  
     printf("Message: \n\t%s\n\n", data_received);
+    printf("onReceived() call exit...\n");
+
 }
 
 /* MISC Functions. */
@@ -146,6 +200,7 @@ void onReceived(const esp_now_recv_info_t *peer_info, const uint8_t *data_receiv
 
 
 void setup() {
+    printf("setup() call entry...\n");
     
     // Init wifi and esp_now.
     if(initWiFi() && initESPNOW()) {
@@ -153,8 +208,6 @@ void setup() {
     }
 
     // Fill peer info.
-   
-
     esp_now_peer_info_t peer = {
         .channel = 6,
         .ifidx = WIFI_IF_STA
@@ -167,8 +220,10 @@ void setup() {
     ESP_ERROR_CHECK(esp_now_add_peer(&peer));
 
     /* For debug: Construct first message. */
-    char data[100];
+    char data[50];
     sprintf(data, "Message #%d: Hello from slave.", ++message_count);
+
+    printf("Sending initial message to greet master...\n");
 
     /* Note: Non-blocking. Don't expect call back to print immediately. */
     esp_now_send(master, (const uint8_t*) data, sizeof(data));
@@ -177,8 +232,11 @@ void setup() {
     // gpio_dump_io_configuration(stdout, SOC_GPIO_VALID_GPIO_MASK);
 
     /* Configure pin to be used. */
-    printf("Configuring pins to be used...\n");
+    printf("Calling pinConfig().\n");
     pinConfig();
+
+    printf("setup() call exit...\n");
+
 }
 
 
@@ -186,12 +244,14 @@ void setup() {
 
 void app_main() {
 
-
+    printf("\n\nCalling setup()...\n");
     setup(); // Set up components to be used.
+
     int sensor_previous_state = 0;
     int sensor_current_state = 0;
 
 
+    printf("Reading sensor level...\n");
     sensor_current_state = gpio_get_level(SENSOR_PIN); /* Read sensor state. */
     printf("Sensor value: %d\n", sensor_current_state);
 
@@ -217,24 +277,10 @@ void app_main() {
             
         msg.sensor_status_flag = sensor_current_state; // The first byte of the message must alway contain the sensor state.
         snprintf(msg.message, sizeof(msg.message), "Message #%d: %s" , ++message_count, state);
+        printf("Sending subsequent message...\n");
         esp_now_send(master, (uint8_t *)&msg, sizeof(msg));
     
         // Save state.
         sensor_previous_state = sensor_current_state;
     }
-
-    /* Configure sleep mode to be used. */
-    printf("Configuring sleep mode...\n");
-    sleepConfig();
-    
-    /* For debug. */
-    printf("\n\nEntering Deep-sleep mode.\n\n");
-    ESP_ERROR_CHECK(esp_deep_sleep_try_to_start());
-    
-    // Will be removed since deep-sleep is enabled.
-    // /* Loop. */
-    // while (true) {
-        
-        
-    // }
 }
