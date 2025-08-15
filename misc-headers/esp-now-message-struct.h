f7605cdeb017118bb7c75e868caef0484343433a
Marcellus Von Sacramento <marcellus.sacramento@gmail.com>
2025-08-14 17:36:22 -0700

    Added enum for message flag.

--- misc-headers/esp-now-message-struct.h
+++ misc-headers/esp-now-message-struct.h
@@ -8,8 +8,15 @@ Purpose: This header file will contain various custom esp-message structure as n
 #define ESP_NOW_MESSAGE_STRUCT
 
 typedef struct esp_message {
+	uint8_t flag;
 	uint8_t sensor_read_level;
 	char message[101];
 } esp_message;
+
+typedef enum message_flag {
+	NORMAL_MESSAGE, /* Used for normal communication. Sensor read level can be ignored. */
+	SENSOR_READ, /* Used for sending sensor read level. Sensor read level must not be ignored if this flag is used. */
+	ERROR_BROADCAST
+} message_flag;
 
 #endif /* ESP_NOW_MESSAGE_STRUCT */
